f003d22093e7b466727b3788bdf3af17f5cb7f88
ankit4sv <ankit4sv@gmail.com>
2018-04-14 12:13:21 +0000

    UI

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,5 +1,24 @@
 var app = angular.module('chatApp', ['ngMaterial']);
 
 app.controller('chatController', function ($scope) {
+    $scope.messages = [
+        {
+            'sender': 'USER',
+            'text': 'Hello'
+	},
+        {
+            'sender': 'BOT',
+            'text': ' kya h be ???'
+	},
+        {
+            'sender': 'USER',
+            'text': 'Beta tumse naa ho payega'
+	},
+        {
+            'sender': 'BOT',
+            'text': 'Aise kaise be !'
+	}
+
+	];
 
 });
