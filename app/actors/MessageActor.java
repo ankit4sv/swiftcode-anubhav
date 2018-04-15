7cd5c6dbe541aac88bbf1685d2e81257349293fc
ankit4sv <ankit4sv@gmail.com>
2018-04-15 11:39:13 +0530

    front end

--- app/actors/MessageActor.java
+++ app/actors/MessageActor.java
@@ -6,15 +6,47 @@ import akka.actor.UntypedActor;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import data.FeedResponse;
 import data.Message;
+import data.NewsAgentResponse;
 import services.FeedService;
 import services.NewsAgentService;
 
 import java.util.UUID;
 
 public class MessageActor extends UntypedActor {
+    // Self - Reference the Actor
+    private final ActorRef out;
+
+    public MessageActor(ActorRef out) {
+        this.out = out;
+    }
+
+    // PROPS
+    public static Props props(ActorRef out) {
+        return Props.create(MessageActor.class, out);
+    }
+
+    // Object of Feed Service
+    // Object of NewsAgentService
+    private FeedService feedService = new FeedService();
+    private NewsAgentService newsAgentService = new NewsAgentService();
+    private NewsAgentResponse newsAgentResponse;
+    private FeedResponse feedResponse;
 
     @Override
     public void onReceive(Object message) throws Throwable {
+        // Send back the response
+        ObjectMapper mapper = new ObjectMapper();
+        if (message instanceof String) {
 
+            Message messageObject = new Message();
+            messageObject.Text=(String)message;
+            messageObject.sender=Message.Sender.USER;
+            out.tell(mapper.writeValueAsString(messageObject), self());
+            String query= newsAgentService.getNewsAgentResponse( "Find" + message,UUID.randomUUID()).query;
+            newsAgentResponse = newsAgentService.getNewsAgentResponse(messageObject.Text,UUID.randomUUID());
+            FeedResponse feedResponse=feedService.getFeedByQuery(newsAgentResponse.query);
+            messageObject.Text=(feedResponse.title == null) ? "No results found" : "Showing results for: " + query;
+            messageObject.feedResponse=feedResponse;
+        }
     }
 }
--- app/data/Message.java
+++ app/data/Message.java
@@ -1,5 +1,9 @@
 package data;
 
 public class Message {
+    public String Text;
+    public FeedResponse feedResponse= new FeedResponse();
+    public Sender sender;
+    public enum Sender {USER,BOT};
 
 }
